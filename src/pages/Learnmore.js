851452a06e4db536393031705c808e6081c29eb6
tiagokim <kt19go@gmail.com>
2023-05-27 01:42:05 -0700

    editor

--- src/pages/Learnmore.js
+++ src/pages/Learnmore.js
@@ -75,9 +75,9 @@ export default function LearnMore() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
                 </svg>
             </div>
-            High-Quality Outputs
+            Secure and Private
           </dt>
-          <dd class="mt-2 text-base leading-7 text-gray-600">Produce professional-quality photos with high resolution and stunning details. Ensure your images look their best when printed or shared online.</dd>
+          <dd class="mt-2 text-base leading-7 text-gray-600">Rest assured that your photos and personal information are kept secure and private. We prioritize data protection to ensure your peace of mind.</dd>
         </div>
         <div class="relative pl-16">
           <dt class="text-base font-semibold leading-7 text-gray-900">
@@ -86,9 +86,9 @@ export default function LearnMore() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
               </svg>
             </div>
-            Secure and Private
+            High-Quality Outputs
           </dt>
-          <dd class="mt-2 text-base leading-7 text-gray-600">Rest assured that your photos and personal information are kept secure and private. We prioritize data protection to ensure your peace of mind.</dd>
+          <dd class="mt-2 text-base leading-7 text-gray-600">Produce professional-quality photos with high resolution and stunning details. Ensure your images look their best when printed or shared online.</dd>
         </div>
       </dl>
     </div>
