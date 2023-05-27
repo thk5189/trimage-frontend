d2f9a455b3f3130b074e4694c61ef6f4e03a5ad5
tiagokim <kt19go@gmail.com>
2023-05-27 11:24:01 -0700

    minor change

--- src/pages/Learnmore.js
+++ src/pages/Learnmore.js
@@ -10,7 +10,7 @@ export default function LearnMore() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-400">Enhance Your Photo Experience</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need to create and edit stunning photos</p>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need to store and edit stunning photos</p>
           <p className="mt-6 text-lg leading-8 text-gray-600">Capture, customize, and cherish your precious moments with our intuitive photo album and powerful photo editor. Take your photography skills to the next level.</p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
