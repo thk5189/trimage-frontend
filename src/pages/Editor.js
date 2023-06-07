b86449ea6aeb1b4b81b36e1efa72c0a4e8429a78
tiagokim <kt19go@gmail.com>
2023-06-06 17:25:13 -0700

    editor for netlify

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,51 +1,3 @@
-// import React, { useState } from 'react';
-
-
-// function Editor() {
-//   const [selectedImage, setSelectedImage] = useState(null);
-
-//   const handleImageUpload = (e) => {
-//     const file = e.target.files[0];
-//     const reader = new FileReader();
-
-//     reader.onload = () => {
-//       setSelectedImage(reader.result);
-//     };
-
-//     reader.readAsDataURL(file);
-//   };
-
-//   const handleApplyFilter = () => {
-//     // Apply filter logic here
-//   };
-
-//   const handleSaveImage = () => {
-//     // Save image logic here
-//   };
-
-//   return (
-//     <div>
-//       <h1>Photo Editor</h1>
-
-//       <input type="file" onChange={handleImageUpload} />
-
-//       {selectedImage && (
-//         <div>
-//           <img src={selectedImage} alt="Selected" />
-
-//           <button onClick={handleApplyFilter}>Apply Filter</button>
-//           <button onClick={handleSaveImage}>Save Image</button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Editor;
-
-// import React from 'react';
-// import '../css/Editor.css'; // Import your custom CSS file
-
 import React, { useEffect } from 'react';
 import '../css/Editor.css'; // Import your custom CSS file
 
@@ -60,7 +12,7 @@ const Editor = () => {
     let image = document.querySelector('#image');
 
     let slider = document.querySelectorAll('.slider');
-    let show_value = document.querySelectorAll('.show_value');
+    // let show_value = document.querySelectorAll('.show_value');
 
     let list_options = document.querySelectorAll('ul li');
 
@@ -160,86 +112,86 @@ const Editor = () => {
         satuVal +
         ')';
 
-    clearAll.style.transform = 'translateY(0px)';
-  }
+      clearAll.style.transform = 'translateY(0px)';
+    }
 
   /*handle each option click event*/
-  list_options.forEach((list_option, index) => {
-    list_option.addEventListener('click', function () {
-      if (image.getAttribute('src') === '') {
-        alert('Choose Image First');
-      } else {
-        options.style.transform = 'translateY(0px)';
-
-        if (Edited === true) {
-          canvas.height = image.naturalHeight;
-          canvas.width = image.naturalWidth;
-
-          for (let i = 0; i <= 4; i++) {
-            if (index !== i) {
-              list_options[i].classList.remove('active_option');
-              option[i].classList.remove('active_controller');
-            } else {
-              this.classList.add('active_option');
-              option[i].classList.add('active_controller');
-            }
-          }
+    list_options.forEach((list_option, index) => {
+      list_option.addEventListener('click', function () {
+        if (image.getAttribute('src') === '') {
+          alert('Choose Image First');
         } else {
-          alert('Edit Your Image First');
+          options.style.transform = 'translateY(0px)';
+
+          if (Edited === true) {
+            canvas.height = image.naturalHeight;
+            canvas.width = image.naturalWidth;
+
+            for (let i = 0; i <= 4; i++) {
+              if (index !== i) {
+                list_options[i].classList.remove('active_option');
+                option[i].classList.remove('active_controller');
+              } else {
+                this.classList.add('active_option');
+                option[i].classList.add('active_controller');
+              }
+            }
+          } else {
+            alert('Edit Your Image First');
+          }
         }
-      }
+      });
     });
-  });
 
   /*download image btn click*/
   function Download_btn() {
-    if (image.getAttribute('src') !== '') {
-      if (Edited === true) {
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
-        var jpegUrl = canvas.toDataURL('image/jpg');
+      if (image.getAttribute('src') !== '') {
+        if (Edited === true) {
+          context.drawImage(image, 0, 0, canvas.width, canvas.height);
+          var jpegUrl = canvas.toDataURL('image/jpg');
 
-        const link = document.createElement('a');
-        document.body.appendChild(link);
+          const link = document.createElement('a');
+          document.body.appendChild(link);
 
-        link.setAttribute('href', jpegUrl);
-        link.setAttribute('download', File_Name);
-        link.click();
-        document.body.removeChild(link);
+          link.setAttribute('href', jpegUrl);
+          link.setAttribute('download', File_Name);
+          link.click();
+          document.body.removeChild(link);
+        }
       }
-    }
   }
 
   /*clear or reset range value*/
   clearAll.addEventListener('click', function () {
-    clearAllRangeValue();
-  });
+      clearAllRangeValue();
+    });
 
   function clearAllRangeValue() {
-    image.style.filter = 'none';
-    context.filter = 'none';
+      image.style.filter = 'none';
+      context.filter = 'none';
 
-    for (let i = 0; i <= slider.length - 1; i++) {
-      if (i === 0) {
-        slider[i].value = '100';
-      } else {
-        slider[i].value = '0';
+      for (let i = 0; i <= slider.length - 1; i++) {
+        if (i === 0) {
+          slider[i].value = '100';
+        } else {
+          slider[i].value = '0';
+        }
       }
-    }
 
-    editImage();
-    clearAll.style.transform = 'translateY(150px)';
+      editImage();
+      clearAll.style.transform = 'translateY(150px)';
   }
 
   /*remove image btn click*/
   remove_img_btn.addEventListener('click', function () {
-    image.src = '';
+      image.src = '';
     this.style.display = 'none';
-    choose_image.style.display = 'block';
-    image_holder.style.display = 'none';
-    options.style.transform = 'translateY(80px)';
-    clearAllRangeValue();
-  });
-}, []);
+      choose_image.style.display = 'block';
+      image_holder.style.display = 'none';
+      options.style.transform = 'translateY(80px)';
+      clearAllRangeValue();
+    });
+  }, []);
 
 const handleDownload = () => {
   // Implement the download logic here
@@ -249,80 +201,80 @@ const handleDownload = () => {
 
   return (
     <div className='editor'>
-    <div className="main">
-      <div className="Tools">
-        <ul>
-          <li>
-            <i className="bx bxs-brightness-half"></i>
-            <p>Brightness</p>
-          </li>
-          <li>
-            <i className="bx bxs-brush"></i>
-            <p>Blur</p>
-          </li>
-          <li>
-            <i className="bx bxs-collection"></i>
-            <p>Greyscale</p>
-          </li>
-          <li>
-            <i className="bx bxs-color-fill"></i>
-            <p>Hue Rotate</p>
-          </li>
-          <li>
-            <i className="bx bxs-magic-wand"></i>
-            <p>Saturation</p>
-          </li>
-          <li onClick={handleDownload}>
-            <i className="bx bx-export"></i>
-            <p>Export</p>
-          </li>
-        </ul>
-      </div>
-      <div className="content">
-        <p id="logo">Photo Editor</p>
-        <div className="choose_image">
-          <div className="upload_img_box">
+      <div className="main">
+        <div className="Tools">
+          <ul>
+            <li>
+              <i className="bx bxs-brightness-half"></i>
+              <p>Brightness</p>
+            </li>
+            <li>
+              <i className="bx bxs-brush"></i>
+              <p>Blur</p>
+            </li>
+            <li>
+              <i className="bx bxs-collection"></i>
+              <p>Greyscale</p>
+            </li>
+            <li>
+              <i className="bx bxs-color-fill"></i>
+              <p>Hue Rotate</p>
+            </li>
+            <li>
+              <i className="bx bxs-magic-wand"></i>
+              <p>Saturation</p>
+            </li>
+            <li onClick={handleDownload}>
+  <i className="bx bx-export"></i>
+  <p>Export</p>
+</li>
+          </ul>
+        </div>
+        <div className="content">
+          <p id="logo">Photo Editor</p>
+          <div className="choose_image">
+            <div className="upload_img_box">
             <i className="bx bxs-image-add"></i>
             <br />
             <input type="file" name="selectedImage" id="selectedImage" accept="image/jpeg, image/png" />
             <p id="hint">Choose Image from folder</p>
-          </div>
+            </div>
         </div>
         <canvas id="image_canvas"></canvas>
-        <div className="image_holder">
+            <div className="image_holder">
           <button id="remove_img_btn">
             <i className="bx bxs-message-square-x"></i>
           </button>
           <img src="" alt="img" id="image" />
-        </div>
-        <div className="options">
+          </div>
+          <div className="options">
           <div className="option">
             <input type="range" max="200" min="0" value="100" id="brightness" className="slider" />
             <p id="brightVal" className="show_value">100</p>
-          </div>
-          <div className="option">
+            </div>
+            <div className="option">
             <input type="range" max="40" min="0" value="0" id="blur" className="slider" />
             <p id="blurVal" className="show_value">0</p>
-          </div>
-          <div className="option">
+            </div>
+            <div className="option">
             <input type="range" max="100" min="0" value="0" id="greyScale" className="slider" />
             <p id="greyVal" className="show_value">0</p>
-          </div>
-          <div className="option">
+            </div>
+            <div className="option">
             <input type="range" max="100" min="0" value="0" id="hue" className="slider" />
             <p id="hueVal" className="show_value">0</p>
-          </div>
-          <div className="option">
+            </div>
+            <div className="option">
             <input type="range" max="100" min="1" value="1" id="saturation" className="slider" />
             <p id="saturationVal" className="show_value">1</p>
+            </div>
           </div>
-        </div>
         <button id="clearAll">
           <span>Reset</span>
           <i className="bx bx-reset"></i>
-        </button>
-      </div>
-    </div>
+            </button>
+          </div>
+        </div>
     </div>
   );
 };
